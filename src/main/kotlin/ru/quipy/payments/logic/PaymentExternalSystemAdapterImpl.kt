562e7458a38969aa6c84929e6b6e2ddf29fac078
d.a.vasilkov <d.a.vasilkov@tbank.ru>
2025-10-03 01:03:28 +0300

    Произошла ошибка при генерации сообщения.

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalSystemAdapterImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalSystemAdapterImpl.kt
@@ -53,9 +53,15 @@ class PaymentExternalSystemAdapterImpl(
     private val paymentScope = CoroutineScope(Dispatchers.IO)
     private val semaphore = Semaphore(permits = parallelRequests)
 
-    private val httpHandledRequestsTotalCounter = metricBuilder.buildHttpHandledRequestsTotalCounter()
+    private val httpHandledRequestsTotalAllCounter = metricBuilder.buildHttpHandledRequestsTotalCounter()
+    private val httpHandledRequestsTotalAccountCounter = metricBuilder.buildHttpHandledRequestsTotalCounter(properties.accountName)
+    private val httpRequestsTotalAllCounter = metricBuilder.buildHttpRequestsTotalCounter()
+    private val httpRequestsTotalAccountCounter = metricBuilder.buildHttpRequestsTotalCounter(properties.accountName)
 
     override fun performPaymentAsync(paymentId: UUID, amount: Int, paymentStartedAt: Long, deadline: Long) {
+        httpRequestsTotalAllCounter.increment()
+        httpRequestsTotalAccountCounter.increment()
+
         logger.warn(
             "[{}] Submitting payment request for payment {}",
             accountName,
@@ -144,7 +150,8 @@ class PaymentExternalSystemAdapterImpl(
             paymentESService.update(paymentId) {
                 it.logProcessing(false, now(), transactionId, reason = "Request timeout.")
             }
-            httpHandledRequestsTotalCounter.increment()
+            httpHandledRequestsTotalAllCounter.increment()
+            httpHandledRequestsTotalAccountCounter.increment()
         } catch (e: Exception) {
             logger.error(
                 "[{}] Payment failed for txId: {}, payment: {}",
--- src/main/kotlin/ru/quipy/payments/metric/MetricBuilder.kt
+++ src/main/kotlin/ru/quipy/payments/metric/MetricBuilder.kt
@@ -8,13 +8,25 @@ import org.springframework.stereotype.Service
 @Service
 class MetricBuilder(private val meterRegistry: MeterRegistry) {
 
-    fun buildHttpHandledRequestsTotalCounter(): Counter = Counter
+    fun buildHttpHandledRequestsTotalCounter(accountName: String = ALL): Counter = Counter
         .builder(HTTP_HANDLED_REQUESTS_TOTAL)
         .description(TOTAL_NUMBER_OF_HANDLED_HTTP_REQUESTS)
+        .tag(ACC, accountName)
+        .register(meterRegistry)
+
+    fun buildHttpRequestsTotalCounter(accountName: String = ALL): Counter = Counter
+        .builder(HTTP_REQUESTS_TOTAL)
+        .description(TOTAL_NUMBER_OF_HTTP_REQUESTS)
+        .tag(ACC, accountName)
         .register(meterRegistry)
 
     companion object {
+        private const val ACC = "acc"
+        private const val ALL = "all"
+
         private const val HTTP_HANDLED_REQUESTS_TOTAL = "http_handled_requests_total"
         private const val TOTAL_NUMBER_OF_HANDLED_HTTP_REQUESTS = "Total number of handled HTTP requests"
+        private const val HTTP_REQUESTS_TOTAL = "http_requests_total"
+        private const val TOTAL_NUMBER_OF_HTTP_REQUESTS = "Total number of HTTP requests"
     }
 }
