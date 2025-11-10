a420b35ffe0d4c231101dc2dc75a4e127d5df085
d.a.vasilkov <d.a.vasilkov@tbank.ru>
2025-11-10 21:56:57 +0300

    fix: улучшена обработка перегрузки в OrderPayer - Ограничение скорости изменено на 6 токенов в секунду - Добавлена проверка заполненности очереди исполнителя платежей - Исключение при переполнении очереди или превышении лимита запросов

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -13,6 +13,9 @@ import java.util.*
 import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.ThreadPoolExecutor
 import java.util.concurrent.TimeUnit
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.web.client.HttpClientErrorException
 
 @Service
 class OrderPayer {
@@ -28,15 +31,17 @@ class OrderPayer {
         16,
         0L,
         TimeUnit.MILLISECONDS,
-        LinkedBlockingQueue(16000),
+        LinkedBlockingQueue(11),
         NamedThreadFactory("payment-submission-executor"),
         CallerBlockingRejectedExecutionHandler()
     )
 
-    val rateLimiter = TokenBucketRateLimiter(11, 11, 1, TimeUnit.SECONDS)
+    val rateLimiter = TokenBucketRateLimiter(6, 11, 1, TimeUnit.SECONDS)
 
     fun processPayment(orderId: UUID, amount: Int, paymentId: UUID, deadline: Long): Long? {
-        if (!rateLimiter.tick()) {
+        if (!rateLimiter.tick()
+            || paymentExecutor.queue.remainingCapacity() == 0
+        ) {
             return null
         }
 
